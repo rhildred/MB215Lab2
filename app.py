e4f860d966e06ab17ff62dfe4c38b43a01cf463b
Rich Hildred <rhildred@ip-172-31-5-173.us-west-2.compute.internal>
2018-11-20 16:21:13 -0500

    added getting request vars

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask import render_template
 from flask import Response
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/sms",  methods=['POST'])
 def sms():
-    return Response("<Response><Message>sms answered here</Message></Response>", mimetype='text/xml')
+    nPhone = request.form["from"]
+    sMessage = request.form["body"]
+    return Response("<Response><Message>" + sMessage + " sms from " + nPhone + " answered here</Message></Response>", mimetype='text/xml')
 
 # a route where we will display a welcome message via an HTML template
 @app.route("/")
